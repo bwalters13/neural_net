ad9298698f2f7a87aab7688ca3328e4b499d88c7
bwalters13 <blakewalterss@gmail.com>
2023-03-08 16:43:50 -0800

    refactoring

--- neural.py
+++ neural.py
@@ -1,16 +1,16 @@
 filename = "test.txt"
-file_lines = [line.strip().split(",") for line in open(filename)]
-file_lines = [int(x) for x in file_lines[0]]
+file_lines = [int(char) for line in open(filename)
+              for char in line.strip().split(",")]
 network = []
-last_layer = None
+prev_layer = None
 
 for num in file_lines[::-1]:
     layer = []
     for i in range(num):
         layer.insert(0, {"collector": 0.0,
-                      "connections": [] if last_layer == None else last_layer})
+                         "connections": [] if prev_layer == None else prev_layer})
         
-    last_layer = layer
+    prev_layer = layer
     network.append(layer)
 
 network = network[::-1]
