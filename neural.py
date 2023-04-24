db52938854bcff61eaf106571139bdfb409076a4
unknown <blakewalterss@gmail.com>
2023-04-24 10:22:12 -0700

    working on small dataset

--- neural.py
+++ neural.py
@@ -1,8 +1,10 @@
 import pickle
-from random import random
+from random import random, uniform
 import math
 import numpy as np
 import pandas as pd
+
+n_epochs = 10000
 
 # files to get structure and inputs
 struct_filename = "test.txt"
@@ -22,30 +24,28 @@ def sigmoid_derivative(x):
 def backpropagate(network, expected_vals):
     for i in range(len(network)-1, -1, -1):
         current_layer = network[i]
-        errors = []
-        if i != len(network) - 1:
-            for j in range(len(current_layer)):
+        for j in range(len(current_layer)):
+            node = current_layer[j]
+            if i != len(network) - 1:
                 error = 0.
                 for node in network[i + 1]:
                     error += (node.weights[j] * node.delta)
-                errors.append(error)
-        else:
-            for j in range(len(current_layer)):
                 node = current_layer[j]
-                errors.append(node.collector - expected_vals[j])
+                node.delta = error * sigmoid_derivative(node.collector)
+            else:
+                error = 2*(node.collector - expected_vals[j])
+                node.delta = error * sigmoid_derivative(node.collector)
 
-        for j in range(len(current_layer)):
-            node = current_layer[j]
-            node.delta = errors[j] * sigmoid_derivative(node.collector)
-
+ 
+       
 def update_weights(network, inputs, lr):
     for i in range(1, len(network)):
-        if i != 0:
+        if i != 1:
             inputs = [node.collector for node in network[i-1]]
         for node in network[i]:
             for j in range(len(inputs)):
                 node.weights[j] -= lr * node.delta * inputs[j]
-            node.weights[-1] -= lr * node.delta
+            #node.weights[-1] -= lr * node.delta
                     
 
 
@@ -54,7 +54,7 @@ class Node:
     def __init__(self, connections):
         self.connections = connections
         self.collector = 0.0
-        self.weights = [random() for x in
+        self.weights = [uniform(-1, 1) for x in
                         range(len(connections))] if connections else []
         self.delta = 0.0
 
@@ -80,28 +80,31 @@ data = pd.DataFrame(np.array(number_inputs).reshape(-1, len(network[0])+1),
 expected_answers = data.answer.values
 number_inputs = data.drop('answer', axis=1).values
 
-
-
 # set inputs
-for _ in range(40):
-    error = 0
-    for row, expected_val in zip(number_inputs, expected_answers):
-        for i, val in enumerate(row):
-            network[0][i].set_value(val)
-
+def train_network(network, learning_rate, target_error):
+    for epoch in range(n_epochs):
+        sum_error = 0
+        for row, expected_val in zip(number_inputs, expected_answers):
+            for i, val in enumerate(row):
+                network[0][i].set_value(val)
             # adding!!!
             for i in range(1, len(struct_file_lines)):
                 for j in range(len(network[i])):
                     for node, weight in zip(network[i][j].connections,
                                             network[i][j].weights):
                         network[i][j].set_value(network[i][j].collector
-                                        + weight * node.collector)
+                                                + weight * node.collector)
                     network[i][j].set_value(sigmoid(network[i][j].collector))
-        error += (network[-1][0].collector - expected_val)**2
-        backpropagate(network, [expected_val])
-        update_weights(network, row, .5)
-    print(f"the error is {error}")
-
+            sum_error += (network[-1][0].collector - expected_val)**2
+            backpropagate(network, [expected_val])
+            update_weights(network, row, learning_rate)
+        
+    
+        print(f"the mean error is {sum_error}")
+        if sum_error < target_error:
+            print(f"Threshold reached in {epoch} epochs")
+            break
+train_network(network, 0.2, 0.05)
 # dump that ish
 with open('network.pkl', 'wb') as f:
     pickle.dump(network, f)
